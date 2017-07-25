af21f8ff04d701c5038ed26727a1f41f9022c70e
ole-savage1012 <30446176+ole-savage1012@users.noreply.github.com>
2017-07-25 19:50:01 +0000

    Add files via upload

---
+++ crazy_script_1.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# This is Shebang bro.
+# This is comment
+#list files
+ls
+
+# redirect the output of ls to "Malory"
+ls > Malory
+
+# Print a blank line
+echo " "
+
+# Print "catting Malory"
+echo "catting Malroy"
+
+# Print the contents of the file "Malory"
+cat Malory
+
+# Print an integer
+echo "1234"
+
+# Print a word
+echo "Word"
+
+
+# print something
+echo "location"
+
+# print current directory
+pwd
+
+# Talk to Jerome
+echo "Jerome! Wassup man. I missed you bro."
+
+
+
+echo " "
+echo " "
+echo "Narrator: The Adventures of Albert: A Life in Play Format. Act 12, Scene 21."
+
+echo " "
+
+echo "Narrator: Our hero commenting on how cold his miserable life is"
+
+echo " "
+
+echo "Albert: I'm cold. Freezing. Hypothermia is setting in. Frostbite has taken my extremeties. I am dying. Forgive me for my sins. Goodbye cruel world."
+
+echo " "
+
+echo "Narrator: The world is cruel is it not?"
+
+echo " "
+
+echo "World: 'Bye Felicia!'"
+
+echo " "
+
+echo "Albert: Well played world, well played."
+
+echo " "
+
+echo "Narrator: A scene taken from William Shakespeare's Julius Caesar"
+
+echo " "
+
+echo "Albert: Et tu Brute?"
+
+echo " "
+
+echo "Albert: So die Albert, die!"
+
+echo " "
+
+echo "Narrator: Ahhhh. Shakespeare is just beautiful is it not?"
+
+echo " "
+
+echo "Narrator: Speaker at the funeral of Albert."
+
+echo " "
+
+echo "Some random dude: 'Albert died. How sad. How unfortunate. How lamentable.'"
+
+echo " "
+
+echo "Narrator: 'Th- th- that's all folks!'"
+
+
+
+
+
+
+
---
+++ test_for_loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+y=0
+
+for ((x=1; x=11; x++))
+do
+    y=$y+$x
+    echo $y
+done
+
